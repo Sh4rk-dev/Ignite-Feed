4a67a1ad2070744c58e12eddfcacc9817aaf4a65
Sh4rk-dev <renanrapace13@gmail.com>
2023-10-21 19:06:39 -0300

    Finalizing the Header and Sidebar Structure and Styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import style from "./Header.module.css"
+import Ignitelogo from "../../../assets/Ignite-logo.svg"
+
+export function Header() {
+    return(
+        <div className="">
+            <header className={style.header}>
+                <img src={Ignitelogo} alt="Logotipo do Ignite" />
+                <h1>Ignite Feed</h1>
+            </header>
+        </div>
+    )
+}
